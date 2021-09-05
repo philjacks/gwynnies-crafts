981e127b8f937aabf728e975f638875c58e1df27
philjacks <77470834+philjacks@users.noreply.github.com>
2021-09-05 17:36:35 +0100

    edits

--- js/index.js
+++ js/index.js
@@ -15,3 +15,18 @@ window.cookieconsent.initialise({
     href: "../pages/docs/privacy.html"
   }
 });
+
+// GOOGLE MAPS
+function initMap() {
+  let options = {
+    zoom: 8,
+    center: { lat: 53.611149, lng: -2.11492 }
+  };
+
+  let map = new google.maps.Map(document.getElementById("map"), options);
+
+  let marker = new google.maps.Marker({
+    position: { lat: 53.611149, lng: -2.11492 },
+    map: map
+  });
+}
